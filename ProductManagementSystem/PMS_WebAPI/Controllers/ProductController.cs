891946126a890045447599aef03726f118f05844
qmdshoeb <qmdshoeb@gmail.com>
2021-05-01 17:19:58 +0530

    get users
    getusers

--- ProductManagementSystem/PMS_WebAPI/Controllers/ProductController.cs
+++ ProductManagementSystem/PMS_WebAPI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PMS_DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,30 @@ namespace PMS_WebAPI.Controllers
 {
     public class ProductController : ApiController
     {
+        ProductManagementSystemEntities db = new ProductManagementSystemEntities
+    }
+
+    public IHttpActionResult orderById()
+    {
+        IList<dbo_Order> orders = null;
+
+        using (var ctx = new ProductManagementSystemEntities())
+        {
+            orders = ctx.Orders.include("")
+
+            .Select(s => new dbo_Order()
+            {
+                Id = s.OrderId,
+                ProductQuantity = s.ProductQuantity,
+                BookingOn = s.BookingOn,
+            }).ToList<dbo_Order>();
+        }
+
+        if (orders.Count == 0)
+        {
+            return NotFound();
+        }
+        return Ok(orders);
+
     }
 }
